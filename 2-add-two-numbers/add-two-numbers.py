57fde1f0650520f5e72595b9ebb032a513300ba5
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2025-11-23 17:51:49 -0500

    Time: 1 ms (81.63%) | Memory: 17.8 MB (84.53%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,30 +1,32 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode()
-        cur = dummy
-        carry = 0
 
-        while l1 or l2 or carry:
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy # point to dummy node
+
+        carry = 0 # this can be either 0 or 1
+
+        while l1 or l2 or carry: # while any of these are non-zero, we must continue
+            
+            # annoying edge case
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
+            
+            value =  v1 + v2 + carry
+            
+            carry = value // 10
+            value = value % 10
 
-            s = v1 + v2 + carry
-            carry, digit = divmod(s, 10)   # same as: carry = s//10; digit = s%10
-
-            cur.next = ListNode(digit)
-            cur = cur.next
+            curr.next = ListNode(value)
+            curr = curr.next
 
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
 
         return dummy.next
+            
