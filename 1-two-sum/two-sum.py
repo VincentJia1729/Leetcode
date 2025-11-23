f98f332800505c01d3167c4d30d2dd92c22b1aeb
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2025-11-23 17:23:23 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.39%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,12 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        seen = {}
-        for i in range (len(nums)):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict = {}
+        
+        for i in range(len(nums)):
             diff = target - nums[i]
-            if diff in seen:
-                return [seen[diff], i]
+
+            if diff not in my_dict:
+                my_dict[nums[i]] = i
             else:
-                seen[nums[i]] = i
+                return [my_dict[diff],i]
+        
