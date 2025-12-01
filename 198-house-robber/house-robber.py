53f6e1b00bf44063547c7b502d2d69204afd3232
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2025-12-01 15:20:51 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (33.56%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,7 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         nums += [0]*2
-        # as the robber, you can either single jump, or double jump
+        # as the robber, you can either jump over 1 empty space, or jump over 2 empty spaces
+        # Ex [1,2,3,4]
+        # If you are at 1, you can either go to 3 or 4
         for i in range(len(nums) - 5, -1, -1):
             nums[i] += max(nums[i+2], nums[i+3])
         return max(nums[0], nums[1])
