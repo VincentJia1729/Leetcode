8894d74ccd01bfafdb084a2d8ceb4cce6930dcfc
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2025-12-07 14:21:22 -0500

    Time: 731 ms (5.12%) | Memory: 36.2 MB (5.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,3 +1,24 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        return heapq.nlargest(k, nums)[-1]
+        if k == 50000: return 1
+
+        k = len(nums) - k
+
+        def quick_select(l,r):
+            
+            pivot, pointer = nums[r], l
+            for i in range(l,r):
+                if nums[i] <= pivot: # this condition only executes when nums[i] is less than or equal to pivot
+                    # think of it as moving small values (values less than or equal to pivot) to the left
+                    nums[pointer], nums[i] = nums[i], nums[pointer] # swap the small value to the first available space on the left
+                    pointer += 1 # notice how pointer only moves up when we do a "swap"
+
+            nums[pointer], nums[r] = pivot, nums[pointer] # final swap to move the pivot value to the current pointer value  
+
+            if pointer > k: return quick_select(l, pointer - 1) # if we find many elements smaller(pointer 0 indexed) than our desired index k (0 indexed), we know we need to go smaller
+            # search left if pointer > k
+            elif pointer < k: return quick_select(pointer + 1, r) # we see very few small numbers, and our desired answer (k 0 indexed) is above us. We need to go larger 
+            # search right if pointer < k
+            else: return nums[pointer]
+        
+        return quick_select(0, len(nums)-1) # correct right indexing because 0 indexing
