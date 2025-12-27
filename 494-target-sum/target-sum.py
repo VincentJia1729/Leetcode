35d9165ce03bb157fe203fcae423618f02a50b98
VincentJia1729 <69536655+VincentJia1729@users.noreply.github.com>
2025-12-26 21:15:01 -0500

    Time: 112 ms (64.34%) | Memory: 17.2 MB (99.48%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -2,13 +2,15 @@
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}
+        
+        dp = defaultdict(int)
+        dp[0] = 1
 
-        def dfs(i,t): # i is the index of nums, "t" is the amount we currently have
-            if i == len(nums): # if we've used all the possible nums in our list
-                return 1 if t == target else 0
-            if (i,t) in dp: # if you've seen this combination before
-                return dp[(i,t)]
-            dp[(i,t)] = dfs(i+1, t + nums[i]) + dfs(i+1, t - nums[i])
-            return dp[(i,t)]
-        return dfs(0,0)
+        for i in range(len(nums)):
+            next_dp = defaultdict(int)
+            for cur_sum, count in dp.items():
+                next_dp[cur_sum + nums[i]] += count
+                next_dp[cur_sum - nums[i]] += count
+            dp = next_dp
+            
+        return dp[target]
